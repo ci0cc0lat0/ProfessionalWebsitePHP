b2239a37da1260cf05a6e6374f28a7eaa7fac8e4
ci0cc0lat0 <anthonymciocco@gmail.com>
2025-03-08 02:19:18 -0600

    Text edits, resume added

--- index.php
+++ index.php
@@ -10,16 +10,16 @@
       <h1>About Me</h1>
       <img src="public/newpfp.png">
       <p>
-      I am a Fullstack developer who is seasoned in JavaScript, NextJS, NodeJS, and Strapi. I like building websites, connecting the pieces together, and bringing projects to life.
+      I am a Fullstack developer who is seasoned in PHP, the LAMP stack, JavaScript, NextJS, NodeJS, and Strapi. I like building websites, connecting the pieces together, and bringing projects to life.
       <br><br>
       I enjoy tinkering with servers and virtual machines, learning new web tools and diving into data analytics.
       <br><br>
       When I am teaching myself something new, I keep myself busy with reading, drawing, playing insturments and gaming. 
       </p>
       <div class="page_about_links">
-      <a href="/AnthonyCioccoFullStackNSM.pdf">Resume</a>
-      <a href="https://github.com/ci0cc0lat0">Github</a>
-      <a href="https://www.linkedin.com/in/anthonymciocco/">LinkedIn</a>
+      <a href="./public/AnthonyCioccoFullStack.pdf" target="_blank">Resume</a>
+      <a href="https://github.com/ci0cc0lat0" target="_blank">Github</a>
+      <a href="https://www.linkedin.com/in/anthonymciocco/" target="_blank">LinkedIn</a>
       </div>
     </div>
   </div>
